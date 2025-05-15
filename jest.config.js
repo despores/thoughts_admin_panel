7dd769627710fbdeb09cb6d03fdd89f0e07bf408
despores <ernest.sakhabutdinov@mail.ru>
2025-05-15 18:52:25 +0300

    fix jest config

--- jest.config.js
+++ jest.config.js
@@ -9,6 +9,7 @@ module.exports = {
   testMatch: [
     "<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}",
     "<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}",
+    "<rootDir>/tests/**/*.{spec,test}.{js,jsx,ts,tsx}",
   ],
   transform: {
     "^.+\\.(ts|tsx)$": "ts-jest",
